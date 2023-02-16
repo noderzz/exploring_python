6ed71d0962528bfb956b86081aebd0dd2ab5c544
Nate <npzaldivar@gmail.com>
2023-02-16 13:20:56 -0700

    Deepening understanding of argparse

--- practicing_argparse.py
+++ practicing_argparse.py
@@ -2,7 +2,12 @@ import argparse
 
 #Create the "ArgumentParser" object and save it as "parser"
 parser = argparse.ArgumentParser()
-parser.add_argument('--input_file', help='Path to input file.')
+
+#Set the flag for the input file along with the help information.
+parser.add_argument('-i', '--input_file', help='Path to input file.')
+
+#Make it so I can access the parsed objects as variables.
 args = parser.parse_args()
 
+#Print off the parsed object variable
 print(f"Input file is {args.input_file}")
