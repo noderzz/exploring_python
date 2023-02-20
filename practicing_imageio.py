38f7df26161821d0674ff6fe8dd45bd7378f66ef
Nate <npzaldivar@gmail.com>
2023-02-20 13:37:23 -0700

    Experimenting with adding audio

--- practicing_imageio.py
+++ practicing_imageio.py
@@ -5,9 +5,13 @@ frame_filenames = []
 files = os.listdir("./frames")
 files = [f for f in files if os.path.isfile(os.path.join("./frames", f))]
 
+# Define the path and filename of the audio file
+audio_file = "./audio/ev.mp3"
+
 # Create a list of the file names of the frames.
 for filename in os.listdir("."):
     frame_filenames = [f"frames/frame_{i:02d}.png" for i in range(1, len(files))]
+
 # Define the output file name and path for the video
 output_file = "video/test_video.mp4"
 
@@ -18,6 +22,12 @@ writer = imageio.get_writer(output_file, fps=30)
 for filename in frame_filenames:
     frame = imageio.imread(filename)
     writer.append_data(frame)
+
+# Load the audio file using imageio
+audio = imageio.get_reader(audio_file, plugin='pydub')
+
+# Add the audio track to the video writer
+writer.add_audio(audio)
 
 # Close the writer to finalize the video file
 writer.close()
