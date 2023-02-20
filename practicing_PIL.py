9b2a5e2ac60d8c3ff751ce4da55d480740fc1d87
Nate <npzaldivar@gmail.com>
2023-02-20 12:20:38 -0700

    Updated practicing PIL script

--- practicing_PIL.py
+++ practicing_PIL.py
@@ -1,18 +1,18 @@
 from PIL import Image
 
 # Load the character image
-character_img = Image.open("character/character.png")
+character_img = Image.open("character/pose0007.png")
 
 # Loop over the mouth images and superimpose them on the character image
-for i in range(1, 31):
+for i in range(1, 23):
     # Load the mouth image
-    mouth_img = Image.open(f"mouths/mouth_{i}.png")
+    mouth_img = Image.open(f"mouths/mouth{i}.png")
     
     # Superimpose the mouth image on the character image
-    character_img.paste(mouth_img, (0, 0), mask=mouth_img)
+    character_img.paste(mouth_img, (310, 435), mask=mouth_img)
     
     # Save the resulting image as a PNG file
-    character_img.save(f"frame_{i:02d}.png")
+    character_img.save(f"frames/frame_{i:02d}.png")
 
     # Reset the character image for the next iteration
-    character_img = Image.open("character.png")
+    character_img = Image.open("character/pose0007.png")
